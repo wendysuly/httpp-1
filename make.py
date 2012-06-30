f03f1831456e6a66d5e65139b60941824da27275
Jaedyn Draper <admin@swkod.com>
2012-06-30 01:45:40 -0500

    Installation addition to makefile.

--- make.py
+++ make.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 
 import jmake
-import sys
 
-jmake.Libraries (
+jmake.Libraries(
    "jnet",
-   "jhash",
    "jformat",
+   "jhash",
    "boost_thread"
 )
 
@@ -19,3 +18,6 @@ jmake.Compiler("g++-4.7")
 jmake.Shared()
 
 jmake.WarnFlags("all", "extra", "ctor-dtor-privacy", "old-style-cast", "overloaded-virtual", "init-self", "missing-include-dirs", "switch-default", "switch-enum", "undef")
+
+jmake.InstallHeaders()
+jmake.InstallOutput()
